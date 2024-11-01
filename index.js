5b14dd05684c6df00562ab1fe6736e6f139f764d
Karla Sosa <karla.sosa@avl.com>
2024-11-01 16:58:37 -0700

    add page preloader

--- index.js
+++ index.js
@@ -13,3 +13,31 @@ function closeImage() {
     imagePopup.style.display = "none";
     document.body.style.overflow = "auto";
 }
+
+// to handle page image loading logic:
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll(".gallery-img");
+    let loadedImagesCount = 0;
+
+    images.forEach((image) => {
+        image.addEventListener("load", () => {
+            loadedImagesCount++;
+            if (loadedImagesCount === images.length) {
+                allImagesLoaded();
+            }
+        });
+
+        // Handle cases where an image is already cached and loaded
+        if (image.complete) {
+            loadedImagesCount++;
+            if (loadedImagesCount === images.length) {
+                allImagesLoaded();
+            }
+        }
+    });
+});
+
+function allImagesLoaded() {
+    document.body.classList.add("gallery-loaded"); // Show gallery
+    document.body.classList.add("preloader-hidden"); // Hide preloader
+}
